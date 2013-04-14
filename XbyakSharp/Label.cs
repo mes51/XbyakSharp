1d6ef3483ddd11e18aef49521860e3c759020dd9
mes <mes.goruba@gmail.com>
2013-04-15 00:38:55 +0900

    add license text

--- XbyakSharp/Label.cs
+++ XbyakSharp/Label.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*!
+ * --- original library license
+ * 
+	@file xbyak.h
+	@brief Xbyak ; JIT assembler for x86(IA32)/x64 by C++
+	@author herumi
+	@url https://github.com/herumi/xbyak, http://homepage1.nifty.com/herumi/soft/xbyak_e.html
+	@note modified new BSD license
+	http://opensource.org/licenses/BSD-3-Clause
+*/
+
+/*
+ * Label.cs
+ * Author: mes
+ * License: new BSD license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
